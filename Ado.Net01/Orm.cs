33efb8354bd057e1d4bef837d2ef9c62c3c8e2da
ayxan777-e <eliyevayxan2006@gmail.com>
2025-12-17 09:31:49 +0400

    created UpdateData method

--- Ado.Net01/Orm.cs
+++ Ado.Net01/Orm.cs
@@ -28,6 +28,46 @@ public class Orm
 
 
 
+    public void UpdateData(string tableName, Dictionary<string, object> data, string whereClause, Dictionary<string, object> whereParams)
+    {
+        // Table yoxlaması (istəyə görə)
+        var allowedTables = new HashSet<string> { "Students", "Teachers", "Courses" };
+        if (!allowedTables.Contains(tableName))
+            throw new Exception("Invalid table name");
+
+        using (SqlConnection conn = new SqlConnection(_connectionString))
+        {
+            conn.Open();
+
+            string setClause = string.Join(", ", data.Keys.Select(k => $"{k} = @{k}"));
+
+            string sql = $"UPDATE {tableName} SET {setClause} WHERE {whereClause}";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                foreach (var item in data)
+                    cmd.Parameters.AddWithValue("@" + item.Key, item.Value ?? DBNull.Value);
+
+                if (whereParams != null)
+                {
+                    foreach (var item in whereParams)
+                    {
+                        if (!data.ContainsKey(item.Key))
+                        {
+                            cmd.Parameters.AddWithValue("@" + item.Key, item.Value ?? DBNull.Value);
+
+                        }
+                    }
+                }
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                Console.WriteLine($"Rows affected: {rowsAffected}");
+            }
+        }
+    }
+
+
+
 
 
 
--- Ado.Net01/Program.cs
+++ Ado.Net01/Program.cs
@@ -6,15 +6,28 @@ Console.WriteLine("Hello, World!");
 
 
 Orm dbcontext = new Orm();
-#region Insert
+//#region Insert
+
+//var data = new Dictionary<string, object>
+//{
+//        {"Name", "Fuad"},
+//        { "Surname", "Agazade" }
+//};
+
+//dbcontext.InsertData("Students", data);
+//#endregion
 
 var data = new Dictionary<string, object>
 {
-        {"Name", "Fuad"},
-        { "Surname", "Agazade" }
+    { "Name", "Fuad" },
+    { "Surname", "Agazade" }
 };
 
-dbcontext.InsertData("Students", data);
-#endregion
+
+var where = new Dictionary<string, object>
+{
+    { "Id", 1 }
+};
 
 
+dbcontext.UpdateData("Students", data, "Id = @Id", where);
