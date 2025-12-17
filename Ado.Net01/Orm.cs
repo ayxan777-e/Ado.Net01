46ed3119fe4389ae3f165dec0579731bf47de3d1
ayxan777-e <eliyevayxan2006@gmail.com>
2025-12-17 09:26:01 +0400

    created InsertData method

---
+++ Ado.Net01/Orm.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+namespace Ado.Net2;
+public class Orm
+{
+
+    private string _connectionString = "Data Source=DESKTOP-H42M0B3\\SQLEXPRESS01;Initial Catalog=DBcontext;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+
+    public void InsertData(string tableName, Dictionary<string, object> data)
+    {
+        using SqlConnection conn = new SqlConnection(_connectionString);
+        conn.Open();
+
+        string columns = string.Join(", ", data.Keys);
+        string values = string.Join(", ", data.Keys.Select(k => "@" + k));
+
+        string sql = $"INSERT INTO {tableName} ({columns}) VALUES ({values})";
+
+        using SqlCommand cmd = new SqlCommand(sql, conn);
+
+        foreach (var item in data)
+        {
+            cmd.Parameters.AddWithValue("@" + item.Key, item.Value ?? DBNull.Value);
+        }
+
+        cmd.ExecuteNonQuery();
+
+    }
+
+
+
+
+
+
+}
---
+++ Ado.Net01/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ado.Net2;
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Orm dbcontext = new Orm();
+#region Insert
+
+var data = new Dictionary<string, object>
+{
+        {"Name", "Fuad"},
+        { "Surname", "Agazade" }
+};
+
+dbcontext.InsertData("Students", data);
+#endregion
+
+
